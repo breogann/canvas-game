434aa37590ac3c1c44231d5764f2576585864983
breogann <fernandocostapradillo@gmail.com>
2022-03-05 02:02:10 +0100

    pff

--- fileloader.js
+++
@@ -1,6 +0,0 @@
-// const image = new Image()
-// image.src = "./img/platform.png"
-
-// // const platformImageUrl = "./img/platform.png"
-// // const backgroundImageUrl = "./img/background.png"
-// // const hillImagerUrl = "./img/hill.png"
